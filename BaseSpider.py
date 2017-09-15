65fcb65255964f6e607e2b21cb7193a874dbb8d6
540366413@qq.com <540366413@qq.com>
2017-09-15 23:28:27 +0800

    modify meitu spider

--- BaseSpider.py
+++ BaseSpider.py
@@ -1,8 +1,27 @@
 from abc import ABC,abstractmethod
 from queue import Queue
 from collections import Iterable
+from threading import local
 import requests
 
+url_map  = local()
+url_map.urls = {}
+
+def isAbsoluteURL(url):
+  if url.startswith("http:") or url.startswith("https:"):
+    return True
+  return False
+
+def formatURL(url, host):
+  if isAbsoluteURL(url):
+    return url
+  else:
+    if url.startswith("/") or host.endswith("/"):
+      return host+url
+    return host+"/"+url
+
+def formatURLs(urls, host):
+  return [ formatURL(url, host) for url in urls]
 
 class Request:
   headers = {
@@ -15,6 +34,10 @@ class Request:
   
   def request(self):
     try:
+      if not url_map.urls.get(self.url,False):
+        url_map.urls[self.url] = True
+      else:
+        return None
       response = requests.get(self.url, headers = self.headers, proxies=self.proxy)
       response.raise_for_status()
       print("{}:{}".format(self.url, response.status_code))
